14d0ecbab62e2ac5bc38ce075e48ed76d8c37e4f
nicole pizzano <pizzano.nicole@gmail.com>
2022-02-04 18:41:50 -0500

    server set up!

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
-// build your server here and require it from index.js
+const express = require('express');
+// const projectRouter = require('./project/router');
+// const resourceRouter = require('./resource/router');
+// const taskRouter = require('./task/router');
+
+const server = express();
+server.use(express.json());
+
+// server.use('/api/projects', projectRouter);
+// server.use('/api/resources', resourceRouter);
+// server.use('/api/tasks', taskRouter);
+
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+server.get('./', (req, res) => {
+    res.send(`<h1> WELCOME TO MY {{{NIGHTMARE}}} AKA: ADDING DATA PERSISTENCE SPRINT CHALLENGE </h1>`)
+})
+
+module.exports = server
+
