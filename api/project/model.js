feb10e7c7cf80ebe42a94980a6c532eede884ad2
nicole pizzano <pizzano.nicole@gmail.com>
2022-02-04 19:59:56 -0500

    migration complete

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,26 @@
-// build your `Project` model here
+/const db = require('../../data/dbConfig.js'); // import the database
+
+function getProjects() { // get all projects
+    return db('projects') // return all projects
+       .select( // select the id, name, and description
+           'project_id', // id
+           'project_name', // name
+           'project_description', // description
+           'project_completed'// completed
+        );
+}
+
+async function createProject(project) { // create a new project
+    const projectId = await db('projects').insert(project); // insert the project into the database
+    const newProject = await getProjects() // get all projects
+        .where('project_id', projectId) // where the id is the id we just inserted
+        .first();   // get the first row
+    return newProject;  // return the new project
+}
+
+module.exports = { // export the functions
+    getProjects, // get all projects
+    createProject // create a new project
+};
+
+
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,25 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+function getResources() {
+    return db('resources') // return all resources from the database
+        .select( // select the id, name, and description
+            'resource_id', 
+            'resource_name', 
+            'resource_description'
+            );
+}
+
+async function createResources(resource) {
+    const resourceId = await db('resources').insert(resource);
+    const newResource = await getResources()
+        .where('resource_id', resourceId)
+        .first();
+    return newResource;
+}
+
+module.exports = {
+    getResources,
+    createResources
+}
+
