e900e09b48c3b7c48c115618f46084a4870e7872
nicole pizzano <pizzano.nicole@gmail.com>
2022-02-06 12:02:06 -0500

    cleaning up comments

--- api/project/model.js
+++ api/project/model.js
@@ -1,26 +1,26 @@
-const db = require('../../data/dbConfig.js'); // import the database
+const db = require('../../data/dbConfig.js'); 
 
-function getProjects() { // get all projects
-    return db('projects') // return all projects
-       .select( // select the id, name, and description
-           'project_id', // id
-           'project_name', // name
-           'project_description', // description
-           'project_completed'// completed
+function getProjects() { 
+    return db('projects') 
+       .select( 
+           'project_id', 
+           'project_name', 
+           'project_description', 
+           'project_completed'
         );
 }
 
-async function createProject(project) { // create a new project
-    const projectId = await db('projects').insert(project); // insert the project into the database
-    const newProject = await getProjects() // get all projects
-        .where('project_id', projectId) // where the id is the id we just inserted
-        .first();   // get the first row
-    return newProject;  // return the new project
+async function createProject(project) { 
+    const projectId = await db('projects').insert(project); 
+    const newProject = await getProjects() 
+        .where('project_id', projectId) 
+        .first();  
+    return newProject;  
 }
 
-module.exports = { // export the functions
-    getProjects, // get all projects
-    createProject // create a new project
+module.exports = { 
+    getProjects, 
+    createProject 
 };
 
 
--- api/project/router.js
+++ api/project/router.js
@@ -2,18 +2,18 @@ const projectRouter = require('express').Router();
 const Projects = require('./model');
 
 projectRouter.get('/', (req, res, next) => {
-       Projects.getProjects() // get all projects
-        .then(projects => { // send them back to the client
-            projects.forEach(row => // for each row in the database, we want to return the id, name, and description
-            !row.project_completed ? // if project is not completed
-                row.project_completed = false : // set project_completed to false
-                row.project_completed = true) // set project_completed to true
-        res.status(200).json(projects); // send projects
+       Projects.getProjects() 
+        .then(projects => { 
+            projects.forEach(row => 
+            !row.project_completed ? 
+                row.project_completed = false : 
+                row.project_completed = true) 
+        res.status(200).json(projects); 
             })
-            .catch (next) 
+            .catch (next) // if error, pass it to the error handler below
 });
 
-// row points to the row in the database
+
 
 projectRouter.post('/', async (req, res, next) => {
     try {
@@ -23,7 +23,7 @@ projectRouter.post('/', async (req, res, next) => {
             newProject.project_completed = true
         res.status(201).json(newProject);
     } catch (error) {
-        next(error);
+        next(error); //
     }
 });
 
